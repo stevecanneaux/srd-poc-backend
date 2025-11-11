f23acec3618487a665f3cb78b0705315108eb6da
Steve Canneaux <seamlessloops@gmail.com>
2025-11-11 04:39:48 +0000

    Update toaster.tsx

--- frontend/components/ui/toaster.tsx
+++ frontend/components/ui/toaster.tsx
@@ -1,1 +1,29 @@
+"use client";
+
+import {
+  Toast,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider>
+      {toasts.map(({ id, title, description }) => (
+        <Toast key={id}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+        </Toast>
+      ))}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
 
